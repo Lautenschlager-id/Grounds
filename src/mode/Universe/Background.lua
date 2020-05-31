c00264ac6cdbf207765bd4cbd8aba3974e05f2c7
Lautenschlager <tai.romani@hotmail.com>
2020-05-31 19:26:41 -0300

    Update Background.lua

--- src/mode/Universe/Background.lua
+++ src/mode/Universe/Background.lua
@@ -4,7 +4,7 @@
 			if i > 0 and i % 8 == 0 then
 				y = y + 1000
 			end
-			tfm.exec.addImage(mode.universe.images.background[i + 1] .. ".png","?" .. i,(i%8) * 1000,y,n)
+			tfm.exec.addImage(mode.universe.images.background[i + 1] .. ".jpg","?" .. i,(i%8) * 1000,y,n)
 
 			if i % 10 == 0 then
 				coroutine.yield()
